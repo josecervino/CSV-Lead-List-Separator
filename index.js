9d318057ff243f2bd4dffbc95d9b037e99a0cd1a
José Cerviño <jciv.public@gmail.com>
2024-06-24 18:55:01 -0400

    update: handle non-standard column names

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const csvPath = '/Users/x/Downloads/5_15_24 - 50k Ecom Stores & Leaders - Verified Emails, Signals, 1-100 employees.csv'; // Replace with target CSV location or provide it as an argument to the file when running with node
+const csvPath = '/Users/x/Cervino Enterprises/Lead Lists/Lead Extractor/6-24-24 - restricted category.csv'; // Replace with target CSV location or provide it as an argument to the file when running with node
 
 if (!csvPath) {
     console.error('Please provide a CSV file path as an argument.');
@@ -59,6 +59,15 @@ function csvToObjects(csvString) {
     return objects;
 }
 
+function convertKeysToLowerCase(obj) {
+    const newObject = {};
+    Object.keys(obj).forEach((key) => {
+        const value = obj[key];
+        newObject[key.toLowerCase().replace(/\s+/g, '_')] = value;
+    });
+    return newObject;
+}
+
 function readCSV(filePath) {
     return new Promise((resolve, reject) => {
         fs.readFile(filePath, 'utf-8', (err, data) => {
@@ -112,36 +121,61 @@ function createListsPerContactType(arrayOfLeadObjects) {
     };
 
     for (let i = 0; i < arrayOfLeadObjects.length; i++) {
-        let lead = arrayOfLeadObjects[i];
-        const leadPhoneNumber = validateAndFormatUSPhoneNumber(lead?.phone_number);
+        let lead = convertKeysToLowerCase(arrayOfLeadObjects[i]);
+        const leadPhoneNumber = validateAndFormatUSPhoneNumber((lead?.phone_number 
+            || lead["Phone Number"] 
+            || lead?.organization_phone_number 
+            || lead["Oragnization Phone Number"]
+        ));
 
         if (i === 0) {
             output.keys = Object.keys(lead);
         }
 
         // format data universally
+        // NAMES
         if (lead?.name && (!lead?.first_name || !lead?.last_name)) {
             const separatedNames = lead.name.split(" ")
+
             lead.first_name = separatedNames[0];
             lead.last_name = separatedNames[separatedNames.length - 1];
         }
-        if (!lead?.email && lead?.personal_email) {
-            lead.email = lead.personal_emaill
+        if (lead["Name"] && (!lead["First Name"] || !lead["Last Name"])) {
+            const separatedNames = lead["Name"].split(" ")
+
+            lead.first_name = separatedNames[0];
+            lead.last_name = separatedNames[separatedNames.length - 1];
         }
-        if (lead.phone_number) { // globally reassign US numbers to formatted number
+        // EMAILS
+        if (!lead?.email && lead["Email"]) {
+            lead.email = lead["Email"];
+        } 
+        if (!lead?.email && lead?.personal_email) {
+            lead.email = lead.personal_email
+        }
+        if (!lead["Email"] && lead["Personal Email"]) {
+            lead.email = lead["Personal Email"]
+        }
+        // PHONE NUMBER
+        if (lead.phone_number || lead["Phone Number"]) { // globally reassign US numbers to formatted number
             if (leadPhoneNumber) {
                 lead.phoneNumber = leadPhoneNumber
             };
         }
 
         // format & assign data per list type
-        if (leadPhoneNumber && lead?.country.includes("United States")) { // if validated US number
+        if (
+            leadPhoneNumber && lead?.country?.includes("United States")
+            || leadPhoneNumber && lead["Country"]?.includes("United States")
+        ) { // if validated US number
             output["US Phone Numbers"].push(lead);
         }
-        if (lead?.personal_email || lead?.email) {
+        if (
+            lead?.personal_email || lead?.email
+        ) {
             output.Emails.push(lead);
         }
-        if (lead?.linkedin_url) {
+        if (lead?.linkedin_url || lead?.linkedin || lead["LinkedIn"]) {
             output.LinkedIn.push(lead);
         }
         if (lead?.organization_twitter_url) {
@@ -151,7 +185,14 @@ function createListsPerContactType(arrayOfLeadObjects) {
             output.Facebook.push(lead);
         }
     }
-
+    // console.log(
+    //     "List",
+    //     output["US Phone Numbers"].length,
+    //     output.Emails.length,
+    //     output.LinkedIn.length,
+    //     output.Facebook.length,
+    //     output.Twitter.length,
+    // )
     return output;
 }
 
@@ -164,7 +205,6 @@ function createCSVsPerContactType(outputObject) {
     const keysToProcess = Object.keys(outputObject).filter(key => key !== "keys");
     const headers = outputObject.keys;
     const formattedHeadersString = outputObject.keys.map(formatColumnName).join(",");
-    console.log(formattedHeadersString);
 
     for (const key of keysToProcess) {
         const data = outputObject[key];
